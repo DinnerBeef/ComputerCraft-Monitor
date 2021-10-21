9d43058a78f78dedcb5cf5fdab87b6f6e2992021
Dinnerbeef <DinnerBeef18@gmail.com>
2021-10-21 14:29:38 -0500

    added color for players

--- src/Players.lua
+++ src/Players.lua
@@ -5,7 +5,7 @@ local chat = peripheral.find("chatBox")
 -- AR Controller
 local ar = peripheral.find("arController")
 
-local name = "J.A.R.V.I.S."
+local chatName = "J.A.R.V.I.S."
 local welcomeON = true
 local welcome = "Welcome To The Base"
 local leaveON = true
@@ -15,12 +15,14 @@ local baseStartX = 5
 local baseStartY = 5
 local baseAddY = 10
 
-local allys = {}
-local enemies = {}
+local stats = {}
 
-local allyColor = 0xF36F38
-local neutralColor = 0xF36F38
-local enemyColor = 0xF36F38
+stats["DinnerBeef"] = ally
+stats["DinnerPoke101"] = enemy
+
+local allyColor = 0x113107
+local neutralColor = 0x0F42EC
+local enemyColor = 0xF30909
 
 local near = {}
 
@@ -37,26 +39,40 @@ function basicARView()
     ar.drawString("Players", startX, startY, 0xF36F38)
 end
 
+function drawPlayer(user, x, y)
+    for table, stat in pairs(stats) do
+        if (table == user) then
+            if (stat == "ally") then
+                ar.drawString(user, x, y, allyColor)
+            elseif (stat == "enemy") then
+                ar.drawString(user, x, y, enemyColor)
+            else
+                ar.drawString(user, x, y, neutralColor)
+            end
+        end
+    end
+end
+
 function showPlayers()
     local allPlayers = getAllPlayers(range)
     for table, user in pairs(allPlayers) do
         if player.isPlayerInRange(range, user) then
             if (near[user] == nil) then
                 startY = startY + addY
-                ar.drawString(user, startX, startY, allyColor)
                 if (welcomeON == true) then
-                    chat.sendMessageToPlayer(welcome, user, name)
+                    drawPlayer(user,startX,startY)
                 end
                 near[user] = user
             end
         end
     end
     for table, user in pairs(near) do
-        if player.isPlayerInRange(range, user) then else
+        if player.isPlayerInRange(range, user) then
+        else
             if (near[user] == nil) then
             else
                 if (leaveON == true) then
-                    chat.sendMessageToPlayer(leave, user, name)
+                    chat.sendMessageToPlayer(leave, user, chatName)
                 end
                 near[user] = nil
                 basicARView()
