e252bf651cbe59d8bfd081f79c194c26ce923003
Dinnerbeef <DinnerBeef18@gmail.com>
2021-11-09 21:54:44 -0600

    Does this work????

--- src/Alarm.lua
+++ src/Alarm.lua
@@ -1,19 +1,27 @@
 local args, args2 = ...
-alarmType = args
-alarmTime = args2
+local alarmType = args
+local alarmTimer = args2
+
+if alarmTimer == nil then
+    alarmTimer = 1
+end
 
 local speakers = { peripheral.find("speaker") }
 
-function playSound (sound, volume, pitch)
-    for _, speaker in ipairs(speakers) do
-        speaker.playSound(sound, volume, pitch)
+function playSound (sound, volume, pitch, duration)
+    for i = 1, duration do
+        for _, speaker in ipairs(speakers) do
+            speaker.playSound(sound, volume, pitch)
+        end
+        sleep(1)
+        duration = duration - 1
     end
 end
 
 if (alarmType == "CodeBlack") then
-    playSound("immersiveengineering:dire_switch",0.5,1.0)
+    playSound("immersiveengineering:dire_switch", 0.5, 1.0, alarmTimer)
 end
 
 if (alarmType == "CodeRed") then
-    playSound("minecraft:entity.wither.death", 1.0, 2.0)
+    playSound("minecraft:entity.wither.death", 1.0, 2.0, alarmTimer)
 end
