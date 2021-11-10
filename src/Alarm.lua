8752eaa14be36f2aeea6e6326bd4b3e4c724ab81
Dinnerbeef <DinnerBeef18@gmail.com>
2021-11-09 21:34:46 -0600

    ALARMS WORK????

--- src/Alarm.lua
+++ src/Alarm.lua
@@ -4,13 +4,16 @@ alarmTime = args2
 
 local speakers = { peripheral.find("speaker") }
 
-function playSound (sound)
+function playSound (sound, volume, pitch)
     for _, speaker in ipairs(speakers) do
-        print("Playing sound " .. sound .. " on speaker " .. speaker)
-        speaker.playSound(sound)
+        speaker.playSound(sound, volume, pitch)
     end
 end
 
+if (alarmType == "CodeBlack") then
+    playSound("immersiveengineering:dire_switch",0.5,1.0)
+end
+
 if (alarmType == "CodeRed") then
-    playSound("immersiveengineering:dire_switch")
+    playSound("minecraft:entity.wither.death", 1.0, 2.0)
 end
