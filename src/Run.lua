23de7d4583449496102f512406439c19e76173b9
Dinnerbeef <DinnerBeef18@gmail.com>
2021-11-10 18:39:51 -0600

    ok redstone will get downloaded this time :)

--- src/Run.lua
+++ src/Run.lua
@@ -5,6 +5,7 @@ programs["Players.lua"] = 1
 programs["Alarm.lua"] = 1
 programs["Ores.lua"] = 1
 programs["Screens.lua"] = 1
+programs["Redstone.lua"] = 1
 
 for table, File in pairs(programs) do -- Updates all the programs
     shell.run("delete " .. table)
