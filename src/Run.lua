d4be49a932b5d646a546140edbe2c44d49613166
Dinnerbeef <DinnerBeef18@gmail.com>
2021-11-12 09:55:28 -0600

    mining will now download

--- src/Run.lua
+++ src/Run.lua
@@ -6,6 +6,7 @@ programs["Alarm.lua"] = 1
 programs["Ores.lua"] = 1
 programs["Screens.lua"] = 1
 programs["Redstone.lua"] = 1
+programs["Mining.lua"] = 1
 
 for table, File in pairs(programs) do -- Updates all the programs
     shell.run("delete " .. table)
