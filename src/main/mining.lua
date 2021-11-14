4586225ebba21df1ca6c117a9e223df612684f55
Dinnerbeef <DinnerBeef18@gmail.com>
2021-11-13 20:14:14 -0600

    fixed mining program

--- src/main/mining.lua
+++ src/main/mining.lua
@@ -2,13 +2,13 @@ local manager = peripheral.find("inventoryManager")
 
 
 function removeItemsMining()
-    manager.removeItemFromPlayer("north", 64 , -1, "minecraft:stone")
-    manager.removeItemFromPlayer("north", 64 , -1, "minecraft:diorite")
-    manager.removeItemFromPlayer("north", 64 , -1, "minecraft:andesite")
-    manager.removeItemFromPlayer("north", 64 , -1, "minecraft:granite")
-    manager.removeItemFromPlayer("north", 64 , -1, "minecraft:dirt")
-    manager.removeItemFromPlayer("north", 64 , -1, "minecraft:gravel")
-    manager.removeItemFromPlayer("north", 64 , -1, "minecraft:cobblestone")
+    manager.removeItemFromPlayer("UP", 64 , -1, "minecraft:stone")
+    manager.removeItemFromPlayer("UP", 64 , -1, "minecraft:diorite")
+    manager.removeItemFromPlayer("UP", 64 , -1, "minecraft:andesite")
+    manager.removeItemFromPlayer("UP", 64 , -1, "minecraft:granite")
+    manager.removeItemFromPlayer("UP", 64 , -1, "minecraft:dirt")
+    manager.removeItemFromPlayer("UP", 64 , -1, "minecraft:gravel")
+    manager.removeItemFromPlayer("UP", 64 , -1, "minecraft:cobblestone")
     sleep(1)
 end
 
